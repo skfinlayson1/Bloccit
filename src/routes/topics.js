24313d319fb2b7041689da519760940c7407cf72
Shane Finlayson <shane_finlayson@yahoo.com>
2019-06-27 17:40:55 -0600

    Validations Exercise: Complete

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const topicController = require("../controllers/topicController");
+const validations = require("./validation");
 
 router.get("/topics", topicController.index);
 router.get("/topics/new", topicController.new);
-router.post("/topics/create", topicController.create);
+router.post("/topics/create", validations.validateTopics, topicController.create);
 router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
 router.get("/topics/:id/edit", topicController.edit);
-router.post("/topics/:id/update", topicController.update);
+router.post("/topics/:id/update", validations.validateTopics, topicController.update);
 
 module.exports = router;
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -14,5 +14,24 @@ module.exports = {
         } else {
             return next();
         }
+    },
+
+    validateTopics(req, res, next) {
+        if(req.method === "POST") {
+            if(req.params.id) {
+                req.checkParams("id", "must be valid").notEmpty().isInt();
+            }
+            req.checkBody("title", "must be at least 5 characters in length").isLength({min: 2});
+            req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
+        }
+
+        const errors = req.validationErrors();
+
+        if (errors) {
+            req.flash('error', errors);
+            return res.redirect(303, req.headers.referer);
+        } else {
+            return next();
+        }
     }
 }
