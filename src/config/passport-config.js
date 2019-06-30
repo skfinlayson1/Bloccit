9a2705b1e052fc8fff600bb912e4915a9483693b
Shane Finlayson <shane_finlayson@yahoo.com>
2019-06-30 16:16:05 -0600

    Authentication: Complete

---
+++ src/config/passport-config.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../db/models").User;
+const authHelper = require("../auth/helpers");
+
+module.exports = {
+
+    init(app) {
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        const userEmail = {usernameField: "email"};
+
+        passport.use(new LocalStrategy(userEmail, (email, password, done) => {
+            User.findOne({
+                where: {email}
+            })
+            .then((user) => {
+                if(!user || !authHelper.comparePass(password, user.password)) {
+                    return done(null, false, {message: "Invalid email or password"});
+                }
+
+                return done(null, user);
+            })
+        }));
+
+        passport.serializeUser((user, callback) => {
+            callback(null, user.id);
+        });
+
+        passport.deserializeUser((id, callback) => {
+            User.findById(id) //have to use findById, findByPk not available until version 5
+            .then((user) => {
+                callback(null, user);
+            })
+            .catch((err) => { //corrected parentheses
+                callback(err, user);
+            })
+        });
+    }
+
+}
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,4 +1,5 @@
 module.exports = {
+
     validatePosts(req, res, next) {
         if(req.method === "POST") {
             req.checkParams("topicId", "must be valid").notEmpty().isInt();
@@ -14,5 +15,23 @@ module.exports = {
         } else {
             return next();
         }
+    },
+
+    validateUsers(req, res, next) {
+        if(req.method === "POST") {
+            req.checkBody("email", "must be valid").isEmail();
+            req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
+            req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+        }
+
+        const errors = req.validationErrors();
+
+        if (errors) {
+            req.flash("error", errors);
+            return res.redirect(req.headers.referer);
+        } else {
+            return next();
+        }
     }
+
 }
