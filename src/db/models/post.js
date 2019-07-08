a9788054235f03bba8bf955fd247653d3bceefe4
Shane Finlayson <shane_finlayson@yahoo.com>
2019-07-07 21:57:49 -0600

    Public_Porfiles: Complete

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -64,7 +64,13 @@ module.exports = (sequelize, DataTypes) => {
 
   Post.prototype.getFavoriteFor = function(userId) {
     return this.favorites.find((favorite) => { return favorite.userId == userId });
-  }
+  };
+
+  Post.addScope("lastFiveFor", (userId) => {
+    return {
+      where: {userId: userId}, limit: 5, order: [["createdAt", "DESC"]]
+    }
+  });
 
   return Post;
 };
--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,8 @@
-const User = require("./models").User;
 const bcrypt = require("bcryptjs");
+
+const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 
 module.exports = {
 
@@ -17,6 +20,34 @@ module.exports = {
         .catch((err) => {
             callback(err);
         })
+    },
+
+    getUser(id, callback) {
+
+        let result = {};
+        User.findById(id)
+        .then((user) => {
+            if(!user) {
+                callback(404);
+            } else {
+                result["user"] = user;
+
+                Post.scope({method: ["lastFiveFor", id]}).findAll()
+                .then((posts) => {
+                    result["posts"] = posts;
+
+                    Comment.scope({method: ["lastFiveFor", id]}).findAll()
+                    .then((comments) => {
+                        result["comments"] = comments;
+                        callback(null, result);
+                    })
+                    .catch((err) => {
+                        callback(err);
+                    });
+                });
+            };
+        });
+
     }
 
 }
